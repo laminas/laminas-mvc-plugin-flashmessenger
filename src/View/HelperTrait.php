5eff3f6aa419b6a75e5c415e2bf97574eebf2d3b
Cvetomir Lazarov <cvetomir.lazarov@gmail.com>
2018-09-05 15:16:18 +0300

    Add doc block with copyright and license information

--- src/View/HelperTrait.php
+++ src/View/HelperTrait.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @see       https://github.com/zendframework/zend-mvc-plugin-flashmessenger for the canonical source repository
+ * @copyright Copyright (c) 2005-2018 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-mvc-plugin-flashmessenger/blob/master/LICENSE.md New BSD License
+ */
 
 namespace Zend\Mvc\Plugin\FlashMessenger\View;
 
